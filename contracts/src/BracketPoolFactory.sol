aaa403b8fd2deda4fd66a98af66a2463c97689ef
hank-butler <hank.butler@protonmail.com>
2026-03-02 21:10:07 -0700

    feat: add token allowlist to BracketPoolFactory

--- contracts/src/BracketPoolFactory.sol
+++ contracts/src/BracketPoolFactory.sol
@@ -5,21 +5,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./BracketPool.sol";
 
 contract BracketPoolFactory is Ownable {
-    address public immutable token;
     address public immutable treasury;
 
+    mapping(address => bool) public allowedTokens;
     address[] public pools;
 
     event PoolCreated(address indexed poolAddress, string poolName, uint256 gameCount);
+    event TokenAdded(address indexed token);
+    event TokenRemoved(address indexed token);
 
-    constructor(address _token, address _treasury) Ownable(msg.sender) {
-        require(_token != address(0), "Invalid token");
+    constructor(address _treasury, address[] memory _initialTokens) Ownable(msg.sender) {
         require(_treasury != address(0), "Invalid treasury");
-        token = _token;
         treasury = _treasury;
+        for (uint256 i = 0; i < _initialTokens.length; i++) {
+            require(_initialTokens[i] != address(0), "Invalid token");
+            allowedTokens[_initialTokens[i]] = true;
+            emit TokenAdded(_initialTokens[i]);
+        }
+    }
+
+    function addToken(address _token) external onlyOwner {
+        require(_token != address(0), "Invalid token");
+        allowedTokens[_token] = true;
+        emit TokenAdded(_token);
+    }
+
+    function removeToken(address _token) external onlyOwner {
+        require(_token != address(0), "Invalid token");
+        require(allowedTokens[_token], "Token not in allowlist");
+        allowedTokens[_token] = false;
+        emit TokenRemoved(_token);
     }
 
     function createPool(
+        address _token,
         string calldata _poolName,
         uint256 _gameCount,
         uint256 _lockTime,
@@ -28,8 +47,10 @@ contract BracketPoolFactory is Ownable {
         uint256 _priceSlope,
         uint256 _maxEntries
     ) external onlyOwner returns (address) {
+        require(allowedTokens[_token], "Token not allowed");
+
         BracketPool pool = new BracketPool(
-            token,
+            _token,
             treasury,
             msg.sender,
             _poolName,
